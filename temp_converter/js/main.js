1b44dd201056daf3de184804f8542b8bf823c2c6
Zack.Lynch <zlynch247@gmail.com>
2016-04-12 09:32:29 -0700

    FINAL COMMIT - wireframes

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,46 @@
+// When the user clicks the #fahrenheit_to_celsius button
+    // Get the value from #temperature and store in variable fahrenheit
+    // Convert that value to a number
+
+    // Convert to celsius and store in variable celsius
+
+    // Display in #result
+
+
+//parseFloat "converts a string to a number"
+
+
+
+    $('#fahrenheit_to_celsius').on('click', function(temperature){
+    	var fahrenheit = $('#temperature').val();
+        fahrenheit = parseFloat(celsius);
+    	var celsius = (fahrenheit - 32) / 1.8;
+        $('celsius').html(number);
+    });
+
+    $('#fahrenheit_to_celsius').on('click', function(temperature){
+    	var celsius = $('#temperature').val();
+    	var celsius = 1.8 * celsius + 32;
+    	return parseFloat(celsius);
+    });
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
