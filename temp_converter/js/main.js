68774ca9f2ef84526063e4021c0434aeaf46b453
Zack.Lynch <zlynch247@gmail.com>
2016-04-12 10:40:13 -0700

    FINAL COMMIT-    TEMP_CONVERTER

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,27 +1,31 @@
-+// When the user clicks the #fahrenheit_to_celsius button
- +    // Get the value from #temperature and store in variable fahrenheit
- +    // Convert that value to a number
- +
- +    // Convert to celsius and store in variable celsius
- +
- +    // Display in #result
- +
- +
- +//parseFloat "converts a string to a number"
- +
- +
- +
- +    $('#fahrenheit_to_celsius').on('click', function(temperature){
- +    	var fahrenheit = $('#temperature').val();
- +        fahrenheit = parseFloat(celsius);
- +    	var celsius = (fahrenheit - 32) / 1.8;
- +        $('celsius').html(number);
- +    });
- +
- +    $('#fahrenheit_to_celsius').on('click', function(temperature){
- +    	var celsius = $('#temperature').val();
- +    	var celsius = 1.8 * celsius + 32;
- +    	return parseFloat(celsius);
- +    });
- +
+// When the user clicks the #fahrenheit_to_celsius button
+// Get the value from #temperature and store in variable fahrenheit
+// Convert that value to a number
+
+// Convert to celsius and store in variable celsius
+
+// Display in #result
+
+
+//parseFloat "converts a string to a number"
+
+
+
+var fahrenheit = 1.8 * celsius + 32;
+var celsius = (fahrenheit - 32) / 1.8;
+
+$('#fahrenheit_to_celsius').on('click', function(temperature){
+		fahrenheit = $('#temperature').val();
+		fahrenheit = parseFloat(celsius);
+		celsius = (fahrenheit - 32) / 1.8;
+		return parseFloat(celsius);
+});
+
+$('#celsius_to_fahrenheit').on('click', function(temperature){
+		celsius = $('#temperature').val();
+		celsius = parseFloat(fahrenheit);
+		fahrenheit = 1.8 * celsius + 32;
+		return parseFloat(fahrenheit);
+});
+
 
