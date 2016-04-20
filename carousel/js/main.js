24b98f6b52ccd35c2f5d72b914b7edb5960a4929
Zack.Lynch <zlynch247@gmail.com>
2016-04-19 18:34:46 -0700

    Carousel (Christine)

--- carousel/js/main.js
+++ carousel/js/main.js
@@ -1,23 +1,36 @@
 // Create an array of image paths
 var images = ["images/image_1.jpg", "images/image_2.jpg", "images/image_3.jpg", "images/image_4.jpg", "images/image_5.jpg", "images/image_6.jpg"];
 // set a variable for our currentPosition
-var currentPosition = images[images.length + 1];
-var lastImage = images[images.length - 1];
+var currentPosition = 0;
+// var lastImage = images[images.length - 1];
 
 // When the user clicks on the next button
 $('#next').on('click', function () {
+    currentPosition = currentPosition + 1;
+    console.log(currentPosition)
+    console.log("HELLO", images[currentPosition])
+    $('#image-to-vote-on').attr('src', images[currentPosition] );
+
+
+
+
+
+
+
+
+
     // add one to the currentPosition
-    if ( (currentPosition) < (lastImage) ) {
-    	images = currentPosition;
-    }
-    // Change the src of #image-to-vote-on to image at that index
-    $('#image-to-vote-on').attr('src', 'images', 'currentPosition' )
-    // // if currentPosition is the last index
-    if (currentPosition) {
-    	currentPosition === [5];
-    // 	 // Disable the next button
-    	document.getElementById('next').disabled = true;
-    }
+    // if ( (currentPosition) < (lastImage) ) {
+    // 	images = currentPosition;
+    // }
+    // // Change the src of #image-to-vote-on to image at that index
+    // $('#image-to-vote-on').attr('src', 'images', 'currentPosition' )
+    // // // if currentPosition is the last index
+    // if (currentPosition) {
+    // 	currentPosition === [5];
+    // // 	 // Disable the next button
+    // 	document.getElementById('next').disabled = true;
+    // }
     // Disable the next button
 });
 // When the user clicks on the previous button
@@ -35,7 +48,7 @@ $('#next').on('click', function () {
 // // When the user clicks on the next button
 // $('#next').on('click', function () {
 //     // add one to the currentPosition
-//     currentPosition = [currentPosition + 1 ];
+    currentPosition = [currentPosition + 1 ];
     
 //     // Change the src of #image-to-vote-on to image at that index
 //     $('#image-to-vote-on').attr('src', 'images', 'currentPosition' )
